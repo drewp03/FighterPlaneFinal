8d76e87b8b657620c5a10a4d85c2580ef765d897
drewp03 <drewprato03@gmail.com>
2025-11-23 20:26:34 -0500

    projectfolder

---
+++ fighterplanefinal/Assets/Scripts/EnemyThree.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyThree : MonoBehaviour
+{
+    public float speed = 2f;
+    public int direction = 1; // 1 = right, -1 = left
+
+    public GameObject explosionPrefab;
+    
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * -direction * -speed * Time.deltaTime, Space.World);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Player")
+        {
+            whatDidIHit.GetComponent<PlayerController>().LoseALife();
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        } else if(whatDidIHit.tag == "Weapons")
+        {
+            Destroy(whatDidIHit.gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            gameManager.AddScore(5);
+            Destroy(this.gameObject);
+        }
+    }
+}
